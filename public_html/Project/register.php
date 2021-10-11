a9541b71cfe64991667b67ccacd637c25aedd0d6
Luke DaSilva <lad5@njit.edu>
2021-10-11 10:24:37 -0400

    Committing Unfinished User Reg

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -1,5 +1,5 @@
 <?php
-    require(__DIR__ . "/../../lib/functions.php");
+    require(__DIR__ . "/../../partials/nav.php");
 ?>
 
 <form onsubmit="return validate(this)" method="POST">
@@ -22,7 +22,6 @@
         //TODO 1: implement JavaScript validation
         //ensure it returns false for an error and true for success
         
-
         return true;
     }
 </script>
@@ -30,10 +29,10 @@
  //TODO 2: add PHP Code
     if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
         // get the email key from $_POST, default to "" if not set and return the value
-        $email = se($_POST["email"], false);
+        $email = se($_POST, "email","", false);
         // same as above but for password and confirm
-        $password = se($_POST["password"], false);
-        $confirm = se($_POST["confirm"], false);
+        $password = se($_POST, "password", "", false);
+        $confirm = se($_POST, "confirm", "", false);
         // TODO 3: validate / use
         $errors = [];
         if (empty($email)) { array_push($errors, "Email must be set."); }
@@ -46,7 +45,17 @@
         if (strlen($password) < 8) { array_push($errors, "Password must be 8 or more characters."); }
         if (strlen($password) > 0 && $password !== $confirm) { array_push($errors, "Passwords don't match."); }
         if (count($errors) > 0) { echo "<pre>" . var_export($errors, true) . "</pre>"; }
-        else { echo "Welcome, $email"; }
-        // TODO 4
+        else {
+            $hash = password_hash($password, PASSWORD_BCRYPT);
+            $db = getDB();
+            $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)");
+            try {
+                $stmt -> execute([":email" => $email, ":password" => $hash]);
+                echo "You've been registered!";
+            } catch (Exception $e) {
+                echo "There was a problem registering";
+                echo "<pre>" . var_export($e, true) . "</pre>";
+            }
+        }
     }
 ?>
