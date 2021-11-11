1891d19adbf950458e7ef6ecade5da5823886a00
Luke DaSilva <lad5@njit.edu>
2021-11-10 20:30:51 -0500

    Inline PHP to the rescue, sorted out nav bar and session var

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -4,9 +4,9 @@ require(__DIR__ . "/../../partials/nav.php");
 <h1>Home</h1>
 <?php
 if (is_logged_in()) {
-    echo "Welcome home, " . get_user_email();
+    echo "Welcome home, " . get_username();
     //comment this out if you don't want to see the session variables
-    echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+    //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 } else {
     echo "You're not logged in";
 }
